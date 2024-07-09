1d859811d86121276042c1091c932c3f7cc75fc2
Nema <shelly.nema@capgemini.com>
2024-07-09 12:23:40 +0530

    comment added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -26,6 +26,7 @@ export default async function Page() {
         <RevenueChart revenue={revenue} />
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
+      {/* <p></p> */}
     </main>
   );
 }
